75fc6ef344b97e6ac30d77c1471e33c8cfb36bf8
nja-006 <lazarus.jason09@gmail.com>
2020-07-30 19:18:00 +0700

    RELEASE 1 DONE

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
     require_once 'animal.php';
+    require_once 'frog.php';
+    require_once 'ape.php';
+
     $sheep = new Animal("shaun");
 
     echo $sheep->get_name().'<br>';
     echo $sheep->get_legs().'<br>';
-    echo $sheep->get_cold_blooded();
+    echo $sheep->get_cold_blooded() . '<br><br>';
 
+    $sungokong = new Ape("kera sakti");
+    $sungokong->yell(); // "Auooo"
+
+    $kodok = new Frog("buduk");
+    $kodok->jump(); // "hop hop"
 
 ?>
